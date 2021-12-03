eadcbffc97301eadccf9a0433d97d934d8407c25
Jinsujin <sylpid003@gmail.com>
2021-12-03 20:42:44 +0900

    플레이그라운드에서 유닛테스트

--- PlayGrounds/CodingTest.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ PlayGrounds/CodingTest.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,7 +1,33 @@
 //: [Previous](@previous)
 
 import Foundation
+import XCTest
 
-var str = "Hello, playground"
+struct Todo {
+    
+}
+
+
+class TodoTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testTodo() {
+        XCTAssertNotNil(Todo())
+    }
+  
+    func testExample() {
+        XCTAssertEqual("1", "1")
+    }
+}
+
+//defaultTestSuite: 클래스의 모든 테스트에 대한 테스트 케이스가 들어있는 테스트 세트
+// 모든 테스트를 실행한다.
+TodoTests.defaultTestSuite.run()
 
 //: [Next](@next)

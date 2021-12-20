254e771a4001bcdd5e272dc48787a786f74c3e8b
Jinsujin <sylpid003@gmail.com>
2021-12-20 23:02:06 +0900

    FreeSchool 파일 생성, 데이터 타입 이해, 함수 작성

---
+++ PlayGrounds/FreeSchool.playground/Pages/basic-1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,181 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+  # 학습 목표
+ - 기본 데이터 타입 이해하기
+ - 프로그램 실행 흐름 제어
+
+ */
+
+
+/*:
+ ### 연습 문제
+ 
+ //빼기 : substract(number1 : Int, number2 : Int) -> Int
+ //제곱 : square(number : Int) -> Int
+ //나누기 : divide(number : Int, divisor : Int) -> Int
+
+ - 참고:
+ [이미 만들어져 있는 함수들](https://swiftdoc.org/v4.2/func/)
+ */
+
+func substract(number1 : Int, number2 : Int) -> Int {
+    return number1 - number2
+}
+
+func square(number : Int) -> Int {
+    return number * number
+}
+
+func divide(number : Int, divisor : Int) -> Int {
+    return number / divisor
+}
+
+func add(number1 : Int, number2 : Int) -> Int {
+    let sum = number1 + number2
+    return sum
+}
+
+func add(number1 : Int, number2 : Int, number3 : Int) -> Int {
+    return add(number1: number1, number2: number2) + number3
+}
+
+add(number1: 123, number2: 234)
+substract(number1: 456, number2: 123)
+square(number: 8)
+divide(number: 256, divisor: 8)
+
+
+/*:
+ ## 데이터 타입, 자료형
+ - 데이터 값(value): 컴퓨터가 처리하는(계산하는) 데이터 단위와 종류
+    - Int
+    - Float, Double
+    - String
+    - Bool
+ 
+ - 변수variable (mutable): 값을 보관하면서 계속해서 바꿀수 있는 공간 == var
+ - 상수constant (Immutable): 처음 값만 넣고 다시 바꿀 수 없는 공간 == let
+ 
+ - 부동 소수형
+    - Float: 부호(1자리) + 지수(8자리) + 가수(23자리) = 32비트
+    - Double: 부호(1자리) + 지수(11자리) + 가수(52자리) = 64비트
+ */
+
+/*:
+ ### 연습 문제
+ 1. inch를 cm로 바꾸는 함수
+ - convertToCM(fromInches : Float) -> Float
+ 
+2. cm 를 inch로 바꾸는 함수
+ - convertToInches(fromCM : Float) -> Float
+ 
+ 3. m를 inch로 바꾸는 함수
+ - convertToInches(fromMeter : Float) -> Float
+ 
+ */
+
+func convertToCM(fromInches : Float) -> Float {
+    return fromInches * 2.54
+}
+
+func convertToInches(fromCM : Float) -> Float {
+    return fromCM / 2.54
+}
+
+func convertToInches(fromMeter : Float) -> Float {
+    return fromMeter * 39.37
+}
+
+
+/*:
+ ## 연산자
+- 기본 자료형을 계산하기 위해 사용한다.
+ - +, -, *, /, &&, ||, ??
+ - %: 나머지 연산자. 배수 확인할때 사용한다.
+
+- 연산자 계산 순서: 대부분의 계산은 왼쪽에서 오른쪽으로 진행된다.
+    1. 단항 연산자 +, -, !, ~
+    2. 산술 연산자 *, /, %
+    3. 산술 연산자 +, -
+ 
+ */
+
+// 단항 연산자
+var intValue = -10
+let result = +intValue // 변수 * (1)
+let minus = -intValue // 변수 * (-1)
+
+
+
+/*:
+ ### 연습 문제
+ 미션1. Rectangle
+ - Playground로 Rectangle struct를 만들어보세요.
+ - Int 형 leftTopX, leftTopY 변수를 추가하세요.
+ - Int 형 rightBottomX, rightBottomY 변수를 추가하세요.
+ - 4개의 꼭지점을 출력하는 printRect() 메서드를 선언하세요.
+ 
+ 미션2. 호출하기
+ 아래에서 Rectangle struct를 rect 변수로 선언하고
+ 기본 제공하는 leftTopX, leftTopY, rightBottomX, rightBottomY 모두 초기화하는 메서드로 값을 지정하세요.
+ printRect() 메서드를 호출해서 꼭지점 좌표를 출력하세요.
+ 
+ 미션3. printArea()
+ 사각형의 면적을 계산해서 출력하는 printArea() 메서드를 추가하세요.
+ 아래에서 printArea() 메서드를 호출하세요.
+ 
+ 미션4. Float형
+ 사각형 좌표를 Float 형으로 바꿔보세요.
+ printRect() 메서드를 호출하세요.
+ */
+
+
+struct Rectangle {
+    var leftTopX: Float
+    var leftTopY: Float
+    var rightBottomX: Float
+    var rightBottomY: Float
+    
+    func printRect() {
+        print(
+        """
+        (\(leftTopX),\(leftTopY))------(\(leftTopX),\(rightBottomY))
+        |       |
+        |       |
+        (\(leftTopX),\(rightBottomY))------(\(rightBottomX),\(rightBottomY))
+        """
+        )
+    }
+    
+    /// 면적 구하기
+    func printArea() {
+        let h = leftTopY - rightBottomY
+        let w = rightBottomX - leftTopX
+        print("면적 = ", w * h)
+    }
+    
+    /// 중점 구하기
+    func printCenter() {
+        let h = leftTopY - rightBottomY
+        let w = rightBottomX - leftTopX
+        
+        let centerX =  w / 2 + leftTopX
+        let centerY = h / 2 + rightBottomY
+        print("중점 = \(centerX), \(centerY)")
+    }
+}
+
+
+var rect = Rectangle(leftTopX: 5, leftTopY: 11, rightBottomX: 15, rightBottomY: 4)
+rect.printRect()
+
+
+var floatRect = Rectangle(leftTopX: 4.5, leftTopY: 10.5, rightBottomX: 15, rightBottomY: 3.5)
+floatRect.printRect()
+floatRect.printCenter()
+
+
+//: [Next](@next)
